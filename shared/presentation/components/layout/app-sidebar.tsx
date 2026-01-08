707faad5bd72e58851b6a7b539ee2d49c13e234c
Berkani oussama ala eddine <berkanioussama@gmail.com>
2026-01-09 00:12:41 +0100

    Fix sidebar delay appear.

--- shared/presentation/components/layout/app-sidebar.tsx
+++ shared/presentation/components/layout/app-sidebar.tsx
@@ -1,15 +1,9 @@
-'use client'
-
-import { useUser } from "@clerk/nextjs";
-import { NavUser } from "@/shared/presentation/components/layout/nav-user";
-import { Sidebar, SidebarHeader,SidebarContent, SidebarFooter } from "@/shared/presentation/components/ui/sidebar";
+import { Sidebar, SidebarHeader,SidebarContent } from "@/shared/presentation/components/ui/sidebar";
 import { Users, Home } from "lucide-react";
 import NavPages from "@/shared/presentation/components/layout/nav-pages";
-import { useFindUserByProviderId } from "@/modules/user/presentation/hooks/use-find-user-by-provider-id.hook";
+import LoginUser from "@/shared/presentation/components/layout/login-user";
 
 const AppSidebar = () => {
-    const { user } = useUser();
-    if (!user) return null;
 
     const pages = [
         { name: "Home", url: "/", icon: Home },
@@ -26,7 +20,7 @@ const AppSidebar = () => {
             <SidebarContent>
                 <NavPages pages={pages} />
             </SidebarContent>
-            <LoginUser providerId={user.id} />
+            <LoginUser />
             
         </Sidebar>
     );
@@ -34,21 +28,3 @@ const AppSidebar = () => {
  
 export default AppSidebar;
 
-const LoginUser = ({providerId}: {providerId: string}) => {
-
-    const {data: user, isLoading, error} = useFindUserByProviderId({ providerId })
-
-    if (isLoading) return <div className="p-2">Loading...</div>;
-    if (error) return <div className="p-2 text-destructive">Error: {(error as Error).message}</div>;
-    if (!user) return <div className="p-2">User not found</div>;
-
-    return (
-        <SidebarFooter>
-            <NavUser 
-                name= {user.name} 
-                email= {user.email} 
-                avatar={user.image}
-            />
-        </SidebarFooter>
-    );
-}
---
+++ shared/presentation/components/layout/login-user.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { useFindUserByProviderId } from "@/modules/user/presentation/hooks/use-find-user-by-provider-id.hook";
+import { SidebarFooter } from "@/shared/presentation/components/ui/sidebar";
+import { NavUser } from "@/shared/presentation/components/layout/nav-user";
+import { useUser } from "@clerk/nextjs";
+
+const LoginUser = () => {
+    const { user } = useUser();
+    if (!user) return null;
+
+    return (
+        <SidebarFooter>
+            <UserProvider providerId={user?.id} />
+        </SidebarFooter>
+    );
+}
+
+export default LoginUser;
+
+const UserProvider = ({ providerId }: { providerId: string }) => {
+    const {data: user, isLoading, error} = useFindUserByProviderId({ providerId })
+
+    if (isLoading) return <div className="p-2">Loading...</div>;
+    if (error) return <div className="p-2 text-destructive">Error: {(error as Error).message}</div>;
+    if (!user) return <div className="p-2">User not found</div>;
+
+    return (
+        <NavUser 
+            name= {user.name} 
+            email= {user.email} 
+            avatar={user.image}
+        />
+    )
+}
